0b37fcfe2c1115ca24682743bc3b20bb4e4af954
Gero <geronimo@devsar.com>
2019-10-09 12:56:54 -0300

    Importo componente nuevo

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { IgPostComponent } from '../components/ig-post/ig-post.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,6 @@ import { Tab1Page } from './tab1.page';
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page]
+  declarations: [Tab1Page, IgPostComponent]
 })
 export class Tab1PageModule {}
